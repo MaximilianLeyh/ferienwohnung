4b08d68efaa822e043d31a3ed0ab47036027c63b
MaximilianLeyh <maximilianleyh@gmail.net>
2024-07-16 20:58:17 +0200

    first commit

---
+++ layouts/custom.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar expand="lg" theme="light" background-color="light">
+      <Container type="fluid">
+        <NavbarBrand>Ferienwohnung</NavbarBrand>
+        <NavbarToggler />
+        <NavbarCollapse>
+          <NavbarNav>
+            <NavLink to="/" active>Startseite</NavLink>
+            <NavLink to="/gallery">Gallerie</NavLink>
+            <NavLink>Kontakt</NavLink>
+          </NavbarNav>
+        </NavbarCollapse>
+      </Container>
+    </Navbar>
+
+    <slot />
+  </div>
+</template>
